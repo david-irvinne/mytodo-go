2ca95bca54635c5f6c748f07a69ff53c43eade57
david-irvinne <davidneilleen@gmail.com>
2025-09-18 18:52:24 +0700

    feat: integrate mongodb

--- main.go
+++ main.go
@@ -1,84 +1,148 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Todo struct { // json: digunakan untuk serialisasi dan deserialisasi
-	ID int `json:"id"`;
+	ID primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`; // data format mongodb uses
 	Completed bool `json:"completed"`;
 	Body string `json:"body"`;
 }
 
-func main() {
-	fmt.Println("david ganteng");	
-	app := fiber.New();
-	
-	err := godotenv.Load(".env")
-	if  err != nil {
-		log.Fatal("error loading .env file");
+// omitempy membuat field dihilangkan dari output jika nilai field tersebut adalah zero value (nilai default) untuk tipe data tersebut
+
+var collection *mongo.Collection;
+
+func main(){
+	fmt.Println("david ganteng");
+
+	err := godotenv.Load(".env");
+	if err != nil {
+		log.Fatal("error loading .env", err);
 	}
 
-	PORT := os.Getenv("PORT");
+	MONGODB_URI := os.Getenv("MONGODB_URI")	;
+	// opsi konfigurasi
+	clientOptions := options.Client().ApplyURI(MONGODB_URI);
+	// koneksi ke mongodb
+	client, err := mongo.Connect(context.Background(), clientOptions)
 
-	todos := []Todo{}
+	if err != nil { 
+		log.Fatal(err);
+	}
 
-	app.Get("/api/todos", func(c *fiber.Ctx) error {
-		return c.Status(200).JSON(todos)
-	})
+	defer client.Disconnect(context.Background());
 	
-	// create todo 
-	app.Post("/api/todos", func(c *fiber.Ctx) error {
-		todo := Todo{}; // id: 0, completed: false, body: ""
-		if err := c.BodyParser(&todo); err != nil {
+	// cek koneksi
+	err = client.Ping(context.Background(), nil);
+	if err != nil {
+		log.Fatal(err);
+	}
+
+	fmt.Println("connected to mongodb:", MONGODB_URI);
+	collection = client.Database("golang_db").Collection("todos");
+
+	app := fiber.New();
+	app.Get("/api/todos", getTodos);
+	app.Post("/api/todos", createTodo);
+	app.Patch("/api/todos/:id", updateTodo);
+	app.Delete("/api/todos/:id", deleteTodo);
+
+	port := os.Getenv("PORT");
+	if port == "" {
+		port = "5000";
+	}
+	
+	log.Fatal(app.Listen("0.0.0.0:" + port));
+}
+
+func getTodos(c *fiber.Ctx) error {
+	var todos []Todo;
+
+	// filter ada di dalam M{}
+	cursor, err := collection.Find(context.Background(), bson.M{});
+	if err != nil {
+		return err;
+	}
+
+	defer cursor.Close(context.Background());
+	
+	for cursor.Next(context.Background()){
+		var todo Todo;
+		if err := cursor.Decode(&todo); err != nil {
 			return err;
 		}
-		
-		if todo.Body == "" {
-			return c.Status(400).JSON(fiber.Map{"error" : "todo body is required"});
-		}
 
-		todo.ID = len(todos) + 1;
 		todos = append(todos, todo);
+	}
 
-		// resource created
-		return c.Status(201).JSON(todo);
-	})
+	return c.JSON(todos);
+}
+func createTodo(c *fiber.Ctx) error {
+	todo := Todo{};
+	if err := c.BodyParser(&todo); err != nil {
+		return err;
+	}
+
+	if todo.Body == "" {
+		return c.Status(400).JSON(fiber.Map{"error" : "todo body cannot be empty"});
+	}
+
+	insertResult, err := collection.InsertOne(context.Background(), todo);
+
+	if err != nil {
+		return err;
+	}
+	todo.ID = insertResult.InsertedID.(primitive.ObjectID)
+	return c.Status(201).JSON(todo);
+}
 
 
-	// update todo
-	app.Patch("/api/todos/:id", func(c *fiber.Ctx) error{
-		id := c.Params("id");
+func updateTodo(c *fiber.Ctx) error {
+	id := c.Params("id");
+	objectId, err := primitive.ObjectIDFromHex(id);
 
-		for i, todo := range todos {
-			if fmt.Sprint(todo.ID) == id {
-				todos[i].Completed = !todos[i].Completed;
-				return c.Status(200).JSON(todos[i]);
-			}
-		}
-
-		return c.Status(404).JSON(fiber.Map{"error" : "todo not found"});
-	})
-
-	// delete a todo
-	app.Delete("/api/todos/:id", func(c * fiber.Ctx) error{
-		id := c.Params("id");
-		for i, todo := range todos {
-			if fmt.Sprint(todo.ID) == id {
-				// ambil dari index 0 sampai (i-1)
-				// ambil dari index i+1 sampai akhir
-				// ... buat unpack
-				todos = append(todos[:i], todos[i + 1:]...);
-				return c.Status(200).JSON(fiber.Map{"success": true})
-			}
-		}
-		return c.Status(404).JSON(fiber.Map{"msg":  "not found"})
-	})
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "invalid todo ID"});
+	}
 	
-	log.Fatal(app.Listen(":" + PORT));
+	filter := bson.M{"_id": objectId};
+	update := bson.M{"$set": bson.M{"completed": true}}; 
+	_, err = collection.UpdateOne(context.Background(), filter, update);
+	if err != nil { 
+		return nil;
+	}
+	return c.Status(200).JSON(fiber.Map{"success": true});
+}
+
+func deleteTodo(c *fiber.Ctx) error {
+	id := c.Params("id");
+	objectId, err := primitive.ObjectIDFromHex(id);
+
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "invalid todo id"});
+	}
+	
+	filter := bson.M{"_id": objectId};
+	result, err := collection.DeleteOne(context.Background(), filter);
+	
+	if err != nil {
+		return err;
+	}
+	if result.DeletedCount == 0 {
+		return c.Status(404).JSON(fiber.Map{"error": "todo not found"});
+	}
+
+	return c.Status(200).JSON(fiber.Map{"success": true})
 }
